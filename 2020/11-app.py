22403fc627367763bf2a40cfba92dbb8877b8194
alexpsimone <alex.p.simone@gmail.com>
2020-12-20 17:14:35 +0000

    cleaned up index errors but there's a runtime issue

--- 2020/11-app.py
+++ 2020/11-app.py
@@ -104,43 +104,74 @@ def count_filled_neighbors_general(seat_chart, line_idx, seat_idx):
 
     return neighbors
 
-
+# open text file to get data
 if __name__ == '__main__':
     data = open('11-data.txt', 'r')
 
+# instantiate empty lists to fill with seat charts
 old_seat_chart = []
 new_seat_chart = []
 
+# fill old_seat_chart with seed data
 for line in data:
-    line.strip()
-    old_seat_chart.append(line)
+    old_seat_chart.append(line.strip())
 
+# # create new_seat_chart with same empty floor spaces as old_seat_chart
+# # use a unique character value (_) where seats would be, so the lists start out different
+# for line_idx in range(len(old_seat_chart)):
+#     new_seat_chart.append([])
+#     for seat_idx in range(len(line)):
+#         if old_seat_chart[line_idx][seat_idx] == '.':
+#             new_seat_chart[line_idx].append('.')
+#         else:
+#             new_seat_chart[line_idx].append('_')
+
+
+# update new_seat_chart
 while new_seat_chart != old_seat_chart:
 
-    for line_idx, line in old_seat_chart:
-        for seat_idx, seat in line:
+    for line_idx, line in enumerate(old_seat_chart):
+
+        new_seat_chart.append([])
+
+        for seat_idx, seat in enumerate(line):
+            # print(f'line_idx: {line_idx} line: {line} seat_idx: {seat_idx} seat: {seat}')
+
             if line_idx == 0:
-                if (seat_idx == 0 or seat_idx == -1) and seat != '.':
-                    new_seat_chart[line_idx][seat_idx] == '#'
+                if (seat_idx == 0 or seat_idx == (len(line)-1)) and seat != '.':
+                    new_seat_chart[line_idx].append('#')
 
                 else:
                     neighbors = count_filled_neighbors_top_line(old_seat_chart, line_idx, seat_idx)
-                    new_seat_chart[line_idx][seat_idx] = determine_seat_change(neighbors, seat)
+                    new_seat_chart[line_idx].append(determine_seat_change(neighbors, seat))
+            
+            elif line_idx == (len(old_seat_chart) - 1):
 
-            if line_idx == -1:
-
-                if (seat_idx == 0 or seat_idx == -1) and seat != '.':
-                    new_seat_chart[line_idx][seat_idx] == '#'
+                if (seat_idx == 0 or seat_idx == (len(line)-1)) and seat != '.':
+                    new_seat_chart[line_idx].append('#')
                 else:
-                    neighbors = count_filled_neighbors_bottom_line(seat_chart, line_idx, seat_idx)
-                    new_seat_chart[line_idx][seat_idx] = determine_seat_change(neighbors, seat)
+                    neighbors = count_filled_neighbors_bottom_line(old_seat_chart, line_idx, seat_idx)
+                    new_seat_chart[line_idx].append(determine_seat_change(neighbors, seat))
+
             else:
                 if seat_idx == 0:
-                    neighbors = count_filled_neighbors_left_col(seat_chart, line_idx, seat_idx)
-                elif seat_idx == -1:
-                    neighbors = count_filled_neighbors_left_col(seat_chart, line_idx, seat_idx)
+                    neighbors = count_filled_neighbors_left_col(old_seat_chart, line_idx, seat_idx)
+                elif seat_idx == (len(line)-1):
+                    neighbors = count_filled_neighbors_right_col(old_seat_chart, line_idx, seat_idx)
                 else:
-                    neighbors = count_filled_neighbors_general(seat_chart, line_idx, seat_idx)
-                new_seat_chart[line_idx][seat_idx] = determine_seat_change(neighbors, seat)
+                    neighbors = count_filled_neighbors_general(old_seat_chart, line_idx, seat_idx)
 
+                new_seat_chart[line_idx].append(determine_seat_change(neighbors, seat))
 
+    old_seat_chart = new_seat_chart
+    new_seat_chart = []
+    
+
+final_tally = 0
+
+for line in new_seat_chart:
+    for seat in line:
+        if seat == '#':
+            final_tally += 1
+
+print(final_tally)
